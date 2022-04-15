76c8354b5771ab09cc8b6bfe241122c441d96cf7
Maklakov Roman <maklakov@cate.ru>
2022-04-16 01:20:18 +0300

    SAVE/LOAD

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -18,6 +18,9 @@ import draftToHtml from "draftjs-to-html";
 //
 //   render() {
 //     const { editorState } = this.state;
+//     const HTML = {__html:draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+//     let tempState;
+//
 //
 //     return (
 //       <div>
@@ -32,6 +35,24 @@ import draftToHtml from "draftjs-to-html";
 //           disabled
 //           value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
 //         ></textarea>
+//           <button onClick={()=>{
+//               // console.log("editorState", editorState._immutable)
+//               tempState = editorState;
+//           }}>Save</button>
+//           <button onClick={()=>{
+//               console.log(tempState);
+//               console.log(editorState);
+//
+//
+//               // this.onEditorStateChange(tempState);
+//           }}>Load</button>
+//           <div
+//               style={{border: "1px solid red"}}
+//               dangerouslySetInnerHTML={HTML}
+//           >
+//
+//           </div>
+//
 //       </div>
 //     );
 //   }
@@ -39,16 +60,35 @@ import draftToHtml from "draftjs-to-html";
 
 
 
-const TextEditor = () => {
 
+const TextEditor = () => {
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+    const [tempState, setTempState] = useState();
+      const onEditorStateChange = (editorState) => {
+          setEditorState(editorState);
+      };
 
   return (
-      <div>
+      <div className="">
         <Editor
+            editorState={editorState}
             toolbarClassName="toolbarClassName"
             wrapperClassName="wrapperClassName"
             editorClassName="editorClassName"
+            onEditorStateChange={onEditorStateChange}
         />
+
+          <button onClick={()=>{
+              setTempState(editorState);
+          }}>Save</button>
+          <button onClick={()=>{
+              console.log(tempState);
+              console.log(editorState);
+              console.log(editorState);
+
+              setEditorState(tempState)
+
+          }}>Load</button>
       </div>
   );
 };
