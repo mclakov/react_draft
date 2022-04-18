1224bc2c7aa66d36bd0d6ec0f04161067d67b32a
Maklakov Roman <maklakov@cate.ru>
2022-04-18 09:47:15 +0300

    text editor 1.0

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -6,76 +6,17 @@ import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import draftToHtml from "draftjs-to-html";
 import ContentState from "draft-js/lib/ContentState";
 
-// export default class TextEditor extends Component {
-//   state = {
-//     editorState: EditorState.createEmpty(),
-//   };
-//
-//   onEditorStateChange = (editorState) => {
-//     this.setState({
-//       editorState,
-//     });
-//   };
-//
-//   render() {
-//     const { editorState } = this.state;
-//     const HTML = {__html:draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-//     let tempState;
-//
-//
-//     return (
-//       <div>
-//         <Editor
-//           editorState={editorState}
-//           toolbarClassName="toolbarClassName"
-//           wrapperClassName="wrapperClassName"
-//           editorClassName="editorClassName"
-//           onEditorStateChange={this.onEditorStateChange}
-//         />
-//         <textarea
-//           disabled
-//           value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-//         ></textarea>
-//           <button onClick={()=>{
-//               // console.log("editorState", editorState._immutable)
-//               tempState = editorState;
-//           }}>Save</button>
-//           <button onClick={()=>{
-//               console.log(tempState);
-//               console.log(editorState);
-//
-//
-//               // this.onEditorStateChange(tempState);
-//           }}>Load</button>
-//           <div
-//               style={{border: "1px solid red"}}
-//               dangerouslySetInnerHTML={HTML}
-//           >
-//
-//           </div>
-//
-//       </div>
-//     );
-//   }
-// }
-
-
-
 
 const TextEditor = () => {
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
     const [tempState, setTempState] = useState();
     const HTML = {__html:draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-
     const sampleMarkup = '<b>Bold text</b>, <i>Italic text</i><br/ ><br />' + '<a href="http://www.facebook.com">Example link</a>';
     const blocksFromHTML = convertFromHTML(sampleMarkup);
     const state = ContentState.createFromBlockArray(
         blocksFromHTML.contentBlocks,
         blocksFromHTML.entityMap,
     );
-
-
-
     const onEditorStateChange = (editorState) => {
           setEditorState(editorState);
       };
@@ -94,24 +35,11 @@ const TextEditor = () => {
               setTempState(editorState);
           }}>Save</button>
           <button onClick={()=>{
-              // console.log(tempState);
-              // console.log(editorState);
-              // setEditorState(tempState);
-
-
-
-
-              // console.log("draftToHtml", draftToHtml(convertToRaw(editorState.getCurrentContent())));
               setEditorState(EditorState.createWithContent(state));
-
-
           }}>Load</button>
-
-
           <textarea
                     value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
                   ></textarea>
-
           <div
                         style={{border: "1px solid red"}}
                         dangerouslySetInnerHTML={HTML}
